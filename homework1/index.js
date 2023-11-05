d887e17c92aeb5239374b698eb6dc6316f7eb21d
Marko7172 <57323360+Marko7172@users.noreply.github.com>
2023-11-06 01:16:59 +0300

    Add files via upload

--- homework1/index.js
+++ homework1/index.js
@@ -1,5 +1,17 @@
+function fetchPosts() {
+    fetch('https://mocki.io/v1/d5f8030d-7192-4bed-a089-7cbd8a27885f')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response error');
+        }
+        return response.json();
+      })
+      .then(postsData => {
+        console.log(postsData);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
 
-
-app.use(cors({
-    origin: true, credentials: true
-}));
+fetchPosts()
